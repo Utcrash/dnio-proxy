0683e2fb986acdc446d42af17e064d76bedeb0d7
Jugnu Agrawal <jugnu@appveen.com>
2023-09-20 14:54:14 +0530

    dockerhub change

--- scripts/push_hub.sh
+++ scripts/push_hub.sh
@@ -5,33 +5,33 @@ set -e
 TAG=`cat CURRENT_PROXY`
 
 echo "****************************************************"
-echo "datanimbus.io.proxy :: Pushing Image to Docker Hub :: appveen/datanimbus.io.proxy:$TAG"
+echo "datanimbus.io.proxy :: Pushing Image to Docker Hub :: datanimbus/datanimbus.io.proxy:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.proxy:$TAG appveen/datanimbus.io.proxy:$TAG
-docker push appveen/datanimbus.io.proxy:$TAG
+docker tag datanimbus.io.proxy:$TAG datanimbus/datanimbus.io.proxy:$TAG
+docker push datanimbus/datanimbus.io.proxy:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.proxy :: Image Pushed to Docker Hub AS appveen/datanimbus.io.proxy:$TAG"
+echo "datanimbus.io.proxy :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.proxy:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.ui-author:$TAG appveen/datanimbus.io.ui-author:$TAG
-docker push appveen/datanimbus.io.ui-author:$TAG
+docker tag datanimbus.io.ui-author:$TAG datanimbus/datanimbus.io.ui-author:$TAG
+docker push datanimbus/datanimbus.io.ui-author:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.proxy :: Image Pushed to Docker Hub AS appveen/datanimbus.io.ui-author:$TAG"
+echo "datanimbus.io.proxy :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.ui-author:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.ui-appcenter:$TAG appveen/datanimbus.io.ui-appcenter:$TAG
-docker push appveen/datanimbus.io.ui-appcenter:$TAG
+docker tag datanimbus.io.ui-appcenter:$TAG datanimbus/datanimbus.io.ui-appcenter:$TAG
+docker push datanimbus/datanimbus.io.ui-appcenter:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.proxy :: Image Pushed to Docker Hub AS appveen/datanimbus.io.ui-appcenter:$TAG"
+echo "datanimbus.io.proxy :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.ui-appcenter:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.ui-swagger:$TAG appveen/datanimbus.io.ui-swagger:$TAG
-docker push appveen/datanimbus.io.ui-swagger:$TAG
+docker tag datanimbus.io.ui-swagger:$TAG datanimbus/datanimbus.io.ui-swagger:$TAG
+docker push datanimbus/datanimbus.io.ui-swagger:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.proxy :: Image Pushed to Docker Hub AS appveen/datanimbus.io.ui-swagger:$TAG"
+echo "datanimbus.io.proxy :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.ui-swagger:$TAG"
 echo "****************************************************"
